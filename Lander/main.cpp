b38e65c40f9b27e68f081b86d760d3e319328293
“Jessica <jaa83@cam.ac.ukgit config --global user.name Jessica Allengit config --global user.email jaa83@cam.ac.uk>
2025-07-13 14:20:10 +0100

    Initial Commit

---
+++ Lander/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lander
+//
+//  Created by Jessica Allen on 13/07/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
